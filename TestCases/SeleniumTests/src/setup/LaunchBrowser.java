17824817ff30013ab771bbe13e2978b525636293
Sruthi <sruthi.av@capgemini.com>
2017-01-19 12:24:41 +0530

    Update LaunchBrowser.java

--- TestCases/SeleniumTests/src/setup/LaunchBrowser.java
+++ TestCases/SeleniumTests/src/setup/LaunchBrowser.java
@@ -30,7 +30,7 @@ public class LaunchBrowser {
 
         // Start Firefox driver
        driver = new FirefoxDriver(firefoxBinary, null);*/
-		System.setProperty("webdriver.firefox.bin","/usr/local/firefox/firefox");
+		System.setProperty("webdriver.firefox.bin","/usr/local/firefox/");
 		driver=new FirefoxDriver();
 
 		
